756e5923e8ab76d2f4d67de71e9a8f4dd0567dc2
Mild~Life <1921208299@qq.com>
2020-12-10 09:59:41 +0800

    Initial commit

---
+++ venv/sarsa/RL_brain.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+class RL(object):
+    def __init__(self, action_space, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = action_space  # a list
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions)
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0] * len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            )
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        # action selection
+        if np.random.uniform() < self.epsilon:
+            # choose best action
+            state_action = self.q_table.loc[observation, :]
+            # some actions may have the same value, randomly choose on in these actions
+            state_action = state_action.reindex(np.random.permutation(state_action.index))
+            action = state_action.argmax()
+        else:
+            # choose random action
+            action = np.random.choice(self.actions)
+        return action
+
+    def learn(self, *args):
+        pass # 每种的都有点不同, 所以用 pass
+
+class SarsaTable(RL):   # 继承 RL class
+
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        super(SarsaTable, self).__init__(actions, learning_rate, reward_decay, e_greedy)    # 表示继承关系
+
+    def learn(self, s, a, r, s_, a_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        if s_ != 'terminal':
+            q_target = r + self.gamma * self.q_table.loc[s_, a_]  # q_target 基于选好的 a_ 而不是 Q(s_) 的最大值
+        else:
+            q_target = r  # 如果 s_ 是终止符
+        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # 更新 q_table
